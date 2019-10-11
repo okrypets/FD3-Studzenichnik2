e19950d8f2f1ea5f4c6bdaa30feb7c7f935cb8d8
Olga Studzenichnik <okrypets@gmail.com>
2019-10-11 11:45:31 +0300

    RainbowFrame - done

---
+++ RainbowFrame/components/RainbowBlock.js
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import RainbowFrame from './RainbowFrame';
+
+class RainbowBlock extends React.Component {
+
+  static propTypes = {
+      helloText: PropTypes.string,
+  };
+
+ render() {
+   let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+
+    return (
+      <div className='RainbowBlock'>
+
+        <RainbowFrame colors={colors}>
+          <span className='hello'>{this.props.helloText}</span>
+        </RainbowFrame>
+
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default RainbowBlock;
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,31 @@
+﻿import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array,
+  };
+  
+  render() {
+      let colors = this.props.colors.slice();
+      let coloredDiv = colors.reduce((total, currentValue, index, colors) => {
+              total = <div
+                  data = {index}
+                  key={index}
+                  style={{border: `solid 3px ${colors[index]}`, padding: "10px"}}
+                  children = {(index === 0) ? this.props.children : total} />;
+          return total;
+      }, 0);
+
+
+    return (
+        <Fragment>
+        {coloredDiv}
+        </Fragment>
+    );
+  }
+
+}
+
+export default RainbowFrame;
