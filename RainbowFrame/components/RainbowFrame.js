036034d53a7cac62e1110edf583b26d6d5128ad5
Olga Studzenichnik <okrypets@gmail.com>
2019-10-16 23:42:40 +0300

    RainbowFrame - arrow func style

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -1,61 +1,37 @@
 ﻿import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 
-class RainbowFrame extends React.Component {
+const RainbowFrame = props => {
 
-  static propTypes = {
-    colors: PropTypes.array,
-  };
-
-  render() {
-    //let colorsArr = this.props.colors;
-    /*
-    //Reduce Version
-
-    let coloredDiv = colors.reduce((total, currentValue, index, colors) => {
-            total = <div
-                data = {index}
-                key={index}
-                style={{border: `solid 3px ${colors[index]}`, padding: "10px"}}
-                children = {(index === 0) ? this.props.children : total} />;
-        return total;
-    }, 0);
-  */
-
-
-//Recursive Version
-
-
-    if (this.props.colors.length === 0)
-      return this.props.children;
-    else {
-      console.log(this.props.colors.length);
-      console.log(this.props.colors[this.props.colors.length-1]);
-      return (
-      <div style={{border: `3px solid ${this.props.colors[this.props.colors.length-1]}`, padding: '10px'}}>
-        <RainbowFrame colors={this.props.colors.slice(0,-1)}>{this.props.children}</RainbowFrame>
-      </div>
-      )
-    }
-
-/*
-   //For Version
-
-   let code = this.props.children;
-   for (let color of colorsArr) {
-       code = <div key={colorsArr.indexOf(color)} style={{border: `3px solid ${color}`, padding: '10px'}}>{code}</div>
-   }
-*/
-/* clear if For Version or Reduce Version
+  let colors = props.colors;
+  let color = colors[colors.length -1];
+  if (props.colors.length === 0)
+    return props.children;
+  else {
     return (
-        <Fragment>
-          {this.props.children}
-        </Fragment>
-    );
-
- */
+        <div
+            key={colors.indexOf(color)}
+            data-id={colors.indexOf(color)}
+            style={
+              {
+                borderWidth: 5,
+                borderStyle: 'solid',
+                borderColor: color,
+                padding: 10,
+              }
+            }
+        >
+          <RainbowFrame colors={colors.slice(0, -1)}>{props.children}</RainbowFrame>
+        </div>
+    )
   }
 
-}
+};
+
+RainbowFrame.propTypes = {
+  colors: PropTypes.array,
+  children: PropTypes.object,
+};
+
 
 export default RainbowFrame;
